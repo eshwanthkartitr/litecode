ae49b2e66ae64a1c4f7459f3aee398b65acc36c0
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-05-02 13:18:14 +0530

    Time: 15 ms (79.95%) | Memory: 23 MB (52.66%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        int maxK = -1;
+        sort(nums.begin(), nums.end());
 
-        for (int num : nums) {
-            if (freq.count(num)) {
-                freq[num]++;
-            } else {
-                if (freq.count(-num)) {
-                    maxK = max(maxK, abs(num));
-                }
-                freq[num] = 1;
-            }
+        int i = 0;
+        int j = nums.size() - 1;
+        while (i < j && nums[i] < 0 && nums[j] > 0) {
+            if (abs(nums[i]) == nums[j]) return nums[j];
+            if (abs(nums[i]) > nums[j]) i++;
+            else j--;
         }
 
-        return maxK;
+        return -1;
     }
 };
