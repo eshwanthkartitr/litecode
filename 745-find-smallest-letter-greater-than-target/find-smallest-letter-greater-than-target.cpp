6e8c40f4633f8c6c84f5f46a11cac717f8776858
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-05-05 00:09:11 +0530

    Time: 15 ms (29.56%) | Memory: 19.4 MB (74.35%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,8 +1,9 @@
-/* Be Humble */
+/* Be Humble mf \U0001fae1 */
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        char cha = letters[0];
+        /*solution 1 " OWN "*/
+        /*char cha = letters[0];
         reverse(letters.begin(),letters.end());
         for(auto ch:letters){
             if(ch - '0' <= target - '0'){
@@ -10,6 +11,26 @@ public:
             }
             cha = ch;
         }
-        return cha;
+        return cha;*/
+        /* Top solution */
+        int n = letters.size();
+        int l=0;
+        int r=n-1;
+        int mid;
+        int pos = -1;
+        while(l<=r){
+            mid=l+(r-l)/2;
+            if(letters[mid]>target){
+                pos=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        if(pos==-1)
+        return letters[0];
+        return letters[pos];
+    
     }
 };
