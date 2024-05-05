6d1ab7d2fd33641e727c470f4b8a9dc4fe385cb4
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-05-05 08:40:55 +0530

    Time: 3 ms (92.47%) | Memory: 11.3 MB (84.10%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -17,7 +17,6 @@ public:
         node->val=(node->next)->val;
         while((node->next)->next!=NULL){
             (node->next)->val=((node->next)->next)->val;
-            cout<<node->val<<"->";
             node=node->next;
         }
         node->next=NULL;
