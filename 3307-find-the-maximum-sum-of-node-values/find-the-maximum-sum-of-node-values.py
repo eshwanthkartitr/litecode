07b93e23b5f2c6cf0ebbbad0ddbc682b197a9f15
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-05-19 18:12:58 +0530

    Time: 974 ms (41.48%) | Memory: 28.1 MB (50.57%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.py
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.py
@@ -1,17 +1,16 @@
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        sum1 = 0
-        var2=0
-        var1=float("inf")
+        total = sum(nums)
+        delta=[]
         for i in nums:
-            if(i^k > i):
-                sum1 += i^k
-                var1 = min(var1,(i^k)-i)
-                var2+=1
-            else:
-                sum1 += i
-                var1 = min(var1,i-(i^k))
-        if (var2%2== 0):
-            return sum1
-        else:
-            return sum1-var1
+            delta.append((i^k) -i)
+        print(delta)
+        delta.sort(reverse=True)
+        print(delta)
+        
+        for i in range(0,len(delta),2):
+            if i+1>=len(delta):
+                break
+            if delta[i]+delta[i+1] > 0:
+                total+=delta[i]+delta[i+1]
+        return total
