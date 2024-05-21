7d05d6422ebc911433652342aaedcd0fe7da8a65
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-05-21 08:25:14 +0530

    Time: 28 ms (95.44%) | Memory: 16.7 MB (30.01%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -5,4 +5,17 @@ class Solution:
         x = self.recur(s[:-1])
         return x + [[s[-1]]+y for y in x]
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        return self.recur(nums)
+        #return self.recur(nums)
+        re=[]
+        subset=[]
+        def dfs(i):
+            if i>=len(nums):
+                re.append(subset.copy())
+                return 
+            subset.append(nums[i])
+            dfs(i+1)
+
+            subset.pop()
+            dfs(i+1)
+        dfs(0)
+        return re
