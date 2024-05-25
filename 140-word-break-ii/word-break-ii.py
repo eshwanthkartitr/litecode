9c6ccd1b21e0e7bdd83ac1a75e4a3069a73375d9
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-05-25 09:25:46 +0530

    Time: 38 ms (35.17%) | Memory: 16.5 MB (87.62%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -1,12 +1,29 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        # re=[]
+        # cur=[]
+        # def backtrack(i):
+        #     if i >=len(s):
+        #         re.append(" ".join(cur))
+        #     for j in range(i,len(s)):
+        #         w = s[i:j+1]
+        #         if w in wordDict:
+        #             cur.append(w)
+        #             backtrack(j+1)
+        #             cur.pop()
+        # backtrack(0)
+        # return re
+        # with memo
+        memo = defaultdict(list)
         re=[]
         cur=[]
         def backtrack(i):
-            if i >=len(s):
+            
+            if i >= len(s):
                 re.append(" ".join(cur))
+                return 
             for j in range(i,len(s)):
-                w = s[i:j+1]
+                w=s[i:j+1]
                 if w in wordDict:
                     cur.append(w)
                     backtrack(j+1)
