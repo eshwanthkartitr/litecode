5db6275f578fd1986ee6813c639d20e54cefe0ba
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-05-27 12:19:15 +0530

    Time: 41 ms (56.62%) | Memory: 16.4 MB (98.90%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -2,11 +2,10 @@ class Solution:
     def specialArray(self, nums: List[int]) -> int:
         res=-1
         for x in range(len(nums)+1):
-            count = x
             cout=0
             for i in nums:
                 if x <= i:
                     cout+=1
-            if count == cout:
+            if x == cout:
                 return cout
         return res
