1f84597b3caea9e80a522e0e4fbb64f93c3ec108
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-06-01 18:19:48 +0530

    Time: 52 ms (44.71%) | Memory: 16.5 MB (94.52%) - LeetSync

--- 1044-find-common-characters/find-common-characters.py
+++ 1044-find-common-characters/find-common-characters.py
@@ -1,17 +1,8 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        wow=[] 
-        for i in range(len(words)): 
-            wow.append(set(words[i]))
-        mp=defaultdict(list)
-        for i in range(len(wow)):
-            for j in wow[i]:
-                mp[j].append(words[i].count(j))
-        print(mp)
-        re=[]
-        for i in mp.keys():
-            if len(mp[i]) == len(words):
-                for j in range(min(mp[i])):
-                    re.append(i)
-        re.sort()
-        return re
+        wow=[]
+        chars = set(words[0])
+        for char in chars:
+            freq = min([word.count(char) for word in words])
+            wow += freq*[char]
+        return wow
