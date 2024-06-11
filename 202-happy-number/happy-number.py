ea3bc370ec62a21edeecbc286d956316b7dd9a03
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-06-11 17:05:00 +0530

    Time: 38 ms (49.69%) | Memory: 16.6 MB (8.89%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        l=list(map(int, str(n)))
+        m=0
+        while l!=[1]:
+            tmp=0
+            for i in l:
+                tmp += i**2
+            l=list(map(int, str(tmp)))
+            m+=1
+            print(l)
+            if l!=1 and m > 10:
+                return False
+        return True
