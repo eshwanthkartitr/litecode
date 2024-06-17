af1c4d442e8d29553ed11d4979cee3cb418e6a6f
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-06-17 20:57:33 +0530

    Time: 44 ms (7.65%) | Memory: 16.5 MB (72.01%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -11,6 +11,6 @@ class Solution:
         tmp = root.left
         root.left=root.right
         root.right=tmp
-        root.left = self.invertTree(root.left)
-        root.right = self.invertTree(root.right)
+        self.invertTree(root.left)
+        self.invertTree(root.right)
         return root
