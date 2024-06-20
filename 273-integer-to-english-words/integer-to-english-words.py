f691194fd7d3f0a6171bc4c62e4975bd867d7497
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-06-21 03:32:12 +0530

    Time: 37 ms (54.13%) | Memory: 16.5 MB (75.61%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.py
+++ 273-integer-to-english-words/integer-to-english-words.py
@@ -1,25 +1,24 @@
 class Solution:
+    def __init__(self):
+        #initialize arrays
+        self.ones = ['', ' One', ' Two', ' Three', ' Four', ' Five', ' Six', ' Seven', ' Eight', ' Nine', ' Ten', ' Eleven', ' Twelve', ' Thirteen', ' Fourteen', ' Fifteen', ' Sixteen', ' Seventeen', ' Eighteen', ' Nineteen']
+        self.tens = ['', ' Ten', ' Twenty', ' Thirty', ' Forty', ' Fifty', ' Sixty', ' Seventy', ' Eighty', ' Ninety']
+        self.thousands = ['', ' Thousand', ' Million', ' Billion']
+    
+    def helper(self, n: int) -> str:
+        if n < 20:
+            return self.ones[n]
+        elif n < 100:
+            return self.tens[n // 10] + self.helper(n % 10)
+        elif n < 1000:
+            return self.helper(n // 100) + ' Hundred' + self.helper(n % 100)
+        else:
+            for i in range(3, 0, -1):
+                if n >= 1000 ** i:
+                    return self.helper(n // (1000 ** i)) + self.thousands[i] + self.helper(n % (1000 ** i))
+        return ''
+    
     def numberToWords(self, num: int) -> str:
-        ONES = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]  
-        TEENS = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]  
-        TENS = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"] 
-        if num == 0:  
-            return "Zero"  
-        elif num< 0:  
-                return "minus " + numberToWords(abs(num))  
-        elif num< 10:  
-                return ONES[num]  
-        elif num< 20:  
-                return TEENS[num - 10]  
-        elif num< 100:  
-                return TENS[num // 10] + (" " + self.numberToWords(num % 10) if num % 10 != 0 else "")  
-        elif num< 1000:  
-                return ONES[num // 100] + " Hundred" + (" " + self.numberToWords(num % 100) if num % 100 != 0 else "")  
-        elif num< 1000000:  
-                return self.numberToWords(num // 1000) + " Thousand" + (" " + self.numberToWords(num % 1000) if num % 1000 != 0 else "")  
-        elif num< 1000000000:  
-                return self.numberToWords(num // 1000000) + " Million" + (" " + self.numberToWords(num % 1000000) if num % 1000000 != 0 else "")  
-        elif num< 10000000000:  
-                return self.numberToWords(num // 1000000000) + " Billion" + (" " + self.numberToWords(num % 1000000000) if num % 1000000000 != 0 else "")  
-        else:  
-            return ""  
+        if num == 0:
+            return 'Zero'
+        return self.helper(num).lstrip()
