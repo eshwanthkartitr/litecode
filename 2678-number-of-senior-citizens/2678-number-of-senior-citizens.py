4078368f665484dad7051ede70ff7f7bb29e2db5
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-08-01 11:34:08 +0530

    Time: 54 ms (11.67%), Space: 16.7 MB (7.17%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -3,7 +3,7 @@ class Solution:
     def countSeniors(self, details: List[str]) -> int:
         cnt=0
         for i in details:
-            tst = re.findall("[M,F](\d+)",i)
-            if int(tst[0][:2]) > 60:
+            tst = re.findall("[M,F,O](\d+)",i)
+            if int(tst[0][0])*10+int(tst[0][1]) > 60:
                 cnt+=1
         return cnt
