55783fe1feecb2587a1ea81a0b6cf0bdfaca7976
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-08-18 15:34:28 +0530

    Time: 117 ms (41.85%), Space: 16.7 MB (36.75%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,10 +1,12 @@
+# Neetcode help
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
         arr=[1]
+        vist=set()
         for i in range(n-1):
-            val = min(arr)
-            arr.remove(val)
+            val = heapq.heappop(arr)
             for j in [2,3,5]:
-                if val*j not in arr:
-                    arr.append(val*j)
+                if val*j not in vist:
+                    heapq.heappush(arr,val*j)
+                    vist.add(val*j)
         return min(arr) 
