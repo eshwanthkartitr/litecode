4579e90e32c5194b5a89d0947164f53b60f6de90
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-08-18 15:31:32 +0530

    Time: 601 ms (6.11%), Space: 16.6 MB (71.48%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        arr=[1]
+        for i in range(n-1):
+            val = min(arr)
+            arr.remove(val)
+            for j in [2,3,5]:
+                if val*j not in arr:
+                    arr.append(val*j)
+        print(arr)
+        return min(arr) 
