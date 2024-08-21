2a0828b4f162ceeabe34adb13856e9e64edefd7c
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-08-21 18:14:25 +0530

    Time: 2042 ms (5.13%), Space: 20.7 MB (12.45%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.py
+++ 0664-strange-printer/0664-strange-printer.py
@@ -1,13 +1,17 @@
 class Solution:
     def strangePrinter(self, s: str) -> int:
-        @cache
+        dp={}
         def solve(i,j):
+            if (i,j) in dp:
+                return dp[(i,j)]
             if i == j:
                 return 1
             min_val = float('inf')
             for k in range(i,j):
                 min_val = min(min_val,solve(i,k)+solve(k+1,j))
             if s[i]==s[j]:
-                return min_val-1
-            return min_val
+                dp[(i,j)] = min_val-1
+            else:
+                dp[(i,j)] = min_val
+            return dp[(i,j)]
         return solve(0,len(s)-1)
