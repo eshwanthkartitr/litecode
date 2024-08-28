f4de3709bd037ffc6da11cf7e1807c5dcccf313a
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-08-28 22:36:51 +0530

    Time: 2188 ms (47.08%), Space: 125.5 MB (5.06%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,5 +1,6 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        @cache
         def helper(i,j):
             if i<0 or i>=len(grid2) or j<0 or j>=len(grid2[0]):
                 return True
