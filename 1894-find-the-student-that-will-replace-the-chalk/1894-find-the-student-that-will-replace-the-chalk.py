a9eb6f0665604668e16371b640717ae04ab71fe6
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-09-02 17:11:47 +0530

    Time: 597 ms (14.91%), Space: 30.1 MB (59.63%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
@@ -1,7 +1,12 @@
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
         i=0
-        while k>=0:
-            k=k-chalk[i]
-            i=(i+1)%len(chalk)
-        return i-1
+        temp = sum(chalk)
+        divi = k - (k//temp)*temp
+        print(divi)
+        for i in range(len(chalk)):
+            print(divi)
+            divi = divi - chalk[i]
+            if divi<0:
+                return i
+        return i+1
