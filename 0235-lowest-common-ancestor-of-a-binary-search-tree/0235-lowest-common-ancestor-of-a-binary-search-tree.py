7066a5ed8dbb60b6c2bcb38cbf3c1d6b97c87cfa
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-09-03 18:57:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,28 +7,11 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        path1=[]
-        path2=[]
-        def traverse(node,end,path):
-            if not node:
-                return None
-            if node.val==end.val:
-                return path+[node.val]
-            tmp1=traverse(node.left,end,path+[node.val])
-            if tmp1:
-                return tmp1
-            tmp2=traverse(node.right,end,path+[node.val])
-            if tmp2:
-                return tmp2
-        path1=traverse(root,p,path1)
-        path2=traverse(root,q,path2)
-        p1,p2=0,0
-        last_common=None
-        while p1<len(path1) and p2<len(path2):
-            if path1[p1]==path2[p2]:
-                last_common=path1[p1]
-                p1+=1
-                p2+=1
+        while root:
+            if root.val>p.val and root.val>q.val:
+                root=root.left
+            if root.val<p.val and root.val<q.val:
+                root=root.right
             else:
-                break
-        return TreeNode(last_common)
+                return root
+            
