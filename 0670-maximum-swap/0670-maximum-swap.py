085f92b92b92ad35a6cb04cd7fa3862ffafe5cb9
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-09-04 11:16:43 +0530

    Time: 37 ms (41%), Space: 16.5 MB (71.85%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        if len(str(num))==1:
+            return num
+        s=list(str(num))
+        ma=num
+        for i in range(len(s)-1):
+            val = max(s[i+1:])
+            idx = len(s)-1-s[::-1].index(val)
+            if s[i]<s[idx]:
+                ma=max(ma,int("".join(s[:i]+[s[idx]]+s[i+1:idx]+[s[i]]+s[idx+1:])))
+        return ma
