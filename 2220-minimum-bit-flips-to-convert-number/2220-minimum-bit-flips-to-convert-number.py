40eb321e2e4890bed8011b25a47905892f333ec3
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-09-11 13:41:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -2,9 +2,9 @@ class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
         x,y=list(bin(start)[2:]),list(bin(goal)[2:])
         if len(bin(start))>len(bin(goal)):
-            y = ['0'] * (len(bin(start))-len(bin(goal)))+y
+            y = [0] * (len(bin(start))-len(bin(goal)))+y
         else:
-            x=['0'] * (-len(bin(start))+len(bin(goal)))+x
+            x=[0] * (-len(bin(start))+len(bin(goal)))+x
         su=0
         print(x,y)
         for i in range(len(x)):
