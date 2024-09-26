4c0fd550375c9b0aea00fc7ffd159c5c423dd191
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-09-26 20:50:04 +0530

    Time: 234 ms (29.05%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,7 +1,1 @@
-SELECT 
-    x,y,z,
-    CASE
-        WHEN x+y>z and y+z>x and x+z>y THEN 'Yes'
-        ELSE 'No'
-    END AS triangle
-FROM Triangle
+Select x,y,z, (case when x+y>z and x+z>y and z+y>x then 'Yes' else 'No' end) as triangle from Triangle
