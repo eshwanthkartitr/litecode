d1e903e244744b7de6b094847583ea412fa7120c
Eshwanth <111058542+eshwanthkartitr@users.noreply.github.com>
2024-09-26 12:57:05 +0530

    Time: 160 ms (87.41%), Space: 17.3 MB (89.44%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,19 +1,37 @@
-class MyCalendar:
+class Node():
+    def __init__(self,start,end,left=None,right=None):
+        self.start=start
+        self.end=end
+        self.left=left
+        self.right=right
 
+    def insert(self,start,end):
+        if self.start < end and self.end > start:
+            return False
+        if self.start >= end:
+            if self.right is None:
+                self.right = Node(start,end)
+                return True
+            else:
+                return self.right.insert(start,end)
+        elif self.end <= start:
+            if self.left is None:
+                self.left=Node(start,end)
+                return True
+            else:
+                return self.left.insert(start,end)
+        return True
+
+class MyCalendar:
     def __init__(self):
-        self.re=[]
+        self.start = None
 
     def book(self, start: int, end: int) -> bool:
-        if self.re==[]:
-            self.re.append((start,end))
+        if not self.start:
+            self.start = Node(start,end)
             return True
         else:
-            for i in self.re:
-                if start < i[1] and end > i[0]:
-                    return False
-            self.re.append((start,end))
-            print((start,end))
-            return True
+            return self.start.insert(start,end)
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
 # param_1 = obj.book(start,end)
