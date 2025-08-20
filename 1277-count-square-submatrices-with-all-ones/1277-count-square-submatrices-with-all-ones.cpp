73224ea7d11e6a34fb9e230da0bec9cb89e2c86a
Eshwanth Karti T R <111058542+eshwanthkartitr@users.noreply.github.com>
2025-08-20 09:32:20 +0530

    Time: 829 ms (5.01%), Space: 30.5 MB (21.64%) - LeetHub

---
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int m = matrix.size(),n =matrix[0].size();
+        vector<vector<int>> irruku(m,vector<int>(n,0));
+        // For 2x2
+        // T T
+        // T T
+        int tmp = 0;
+        for(int i=0;i<m;i++){
+            if(matrix[i][0] == 1){
+                irruku[i][0] = 1;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(matrix[0][i] == 1){
+                irruku[0][i] = 1;
+            }
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==1){
+                    irruku[i][j] = 1+min(min(irruku[i-1][j-1],irruku[i][j-1]),irruku[i-1][j]);
+                }else{
+                    irruku[i][j] = 0;
+                }
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                cout << irruku[i][j] << " ";
+                tmp += irruku[i][j];
+            }
+            cout << endl;
+        }
+
+        return tmp;
+    }
+};
