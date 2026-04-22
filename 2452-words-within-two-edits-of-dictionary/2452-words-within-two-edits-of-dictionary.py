5ead956fac40cc2d09f875a8a57dbd1804c99592
Eshwanth Karti T R <111058542+eshwanthkartitr@users.noreply.github.com>
2026-04-22 20:32:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
@@ -2,6 +2,7 @@ class Solution:
     def twoEditWords(self, queries: List[str], dic: List[str]) -> List[str]:
         re=[]
         for i in queries:
+            fall = False
             for j in dic:
                 m=0
                 cnt =0
@@ -9,9 +10,13 @@ class Solution:
                 while m<sizee:
                     if i[m]==j[m]:
                         cnt+=1
+                    if cnt>=sizee-2:
+                        fall=True
+                        break
+                        
                     m+=1
                 
-                if cnt>=sizee-2:
+                if fall or cnt>=sizee-2:
                     re.append(i)
                     break
 
